8670554da6d8125f9cb1dd7399a6d8d8bd5db1ae
britonmearsty <mearstybriton@gmail.com>
2024-09-22 06:09:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    const open = document.querySelector(".menu");
+    open.style.display = 'block';
+}
+function hideMenu(){
+    const open = document.querySelector(".menu");
+    open.style.display = 'none';
+}
